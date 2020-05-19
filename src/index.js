699d92608b6c9a3628b81b2153b229a4ae830811
Nicolas Cordin <nicolas@dindon.us>
2020-05-19 10:13:59 +0200

    Show result in CLI, remove json report

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
-const writeJsonFile = require('write-json-file');
-const reducers = require('./reducers');
-const scan = require('./utilities/scan');
-const {addHistory} = require('./utilities/reports');
-const {resolveObjectOfPromises} = require('./utilities/promise');
+const reducers = require("./reducers");
+const scan = require("./utilities/scan");
+const { resolveObjectOfPromises } = require("./utilities/promise");
 
 const [, , projectPath] = process.argv;
 
+// ../webapp-mobile/app
+// ../webapp-desktop/app
+
 if (!projectPath) {
-  console.error('\n*** Failed! ***\n\nUsage: <ProjectPath>\n');
+  console.error("\n*** Failed! ***\n\nUsage: <ProjectPath>\n");
   process.exit(0);
 }
 
 function scanWithReducer(projectPath, reducerName, reducer) {
   console.log(`generating ${reducerName} report...`);
 
-  return scan(projectPath, ['**/*.js', '**/*.html'], reducer).then(report => {
-    console.log(`✓ ${reducerName} done`);
-    return report;
-  });
+  return scan(projectPath, ["**/*.js", "**/*.jsa", "**/*.html"], reducer).then(
+    (report) => {
+      console.log(`✓ ${reducerName} done`);
+      return report;
+    }
+  );
 }
 
-const scanProject = projectPath => {
+const scanProject = (projectPath) => {
   const scanPromises = {};
 
-  Object.keys(reducers).forEach(reducerName => {
+  Object.keys(reducers).forEach((reducerName) => {
     const reducer = reducers[reducerName];
     const promise = scanWithReducer(projectPath, reducerName, reducer);
 
     scanPromises[reducerName] = promise;
   });
 
-  resolveObjectOfPromises(scanPromises).then(reports => {
-    addHistory(reports).then(reportsWithHistory => {
-      writeJsonFile('reports.json', reportsWithHistory);
-      console.log('✓ all reports saved into reports.json');
-    });
+  resolveObjectOfPromises(scanPromises).then((reports) => {
+    const total =
+      reports.summarize.react.lines +
+      reports.summarize.angular.lines +
+      reports.summarize.native.lines;
+    const angularRatio = reports.summarize.angular.lines / total;
+    const migrationPourcent = Math.round((1 - angularRatio) * 10000) / 100;
+
+    console.log(`✓ ${migrationPourcent}% of migration done.`);
   });
 };
 
